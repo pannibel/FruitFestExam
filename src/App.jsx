a66ec38e53e33af92d230c1099c70ea62e9f87eb
Panni Belevski <pannibel@gmail.com>
2022-12-01 13:22:07 +0100

    2 buttons to visit ticket website or festival app

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,21 @@ import Tickets from "./Tickets";
 function App() {
   const [count, setCount] = useState(0);
   const [showLineup, setShowLineup] = useState(false);
+  const [showTickets, setShowTickets] = useState(false);
+
 
   return (
     <>
       {/* mainly mobile */}
       {/* <Lineup /> */}
-      {!showLineup && (
+      {!showLineup && !showTickets && (
         <button onClick={() => setShowLineup(true)}>Check out the app</button>
       )}
+      {!showLineup &&  !showTickets && (
+        <button onClick={() => setShowTickets(true)}>Get tickets</button>
+      )}
       {showLineup && <Lineup />}
-      {!showLineup && <Tickets />}
+      {showTickets && <Tickets />}
       {/* mainly desktop */}
       {/* <Tickets /> */}
     </>
