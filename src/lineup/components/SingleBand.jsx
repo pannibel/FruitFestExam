dac0e6a7a8299e5593d8f70e4e0078f822dd7b29
HoodDoob <hubert.kurkiewicz@gmail.com>
2022-12-03 22:01:54 +0100

    current band palying is working

--- src/lineup/components/SingleBand.jsx
+++ src/lineup/components/SingleBand.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { useState, useEffect } from "react";
 function SingleBand(props) {
   const [likedBand, setLikedBand] = useState(true);
-  function likeBand(value) {
-    setLikedBand(!value);
-    props.data.liked = JSON.stringify(likedBand);
-    console.log(props.data);
+  function likeBand() {
+    setLikedBand(!likedBand);
+    // props.data.liked = JSON.stringify(likedBand);
+    // console.log(likedBand);
   }
 
   return (
     <>
       <h2>{props.data.name}</h2>
       <h3>{props.data.genre}</h3>
-      <h6>{props.data.liked}</h6>
+      <h6>{JSON.stringify(likedBand)}</h6>
       <button onClick={likeBand}>like it</button>
       <h5>{props.data.name}</h5>
       <img src={props.data.logo} alt={props.data.logoCredits}></img>
