7bbb90e96ca990aa2b765960e3add00202c34751
Panni Belevski <pannibel@gmail.com>
2022-12-03 17:32:09 +0100

    basket shows products

--- src/tickets/components/Basket.jsx
+++ src/tickets/components/Basket.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 function Basket(props) {
+
+// console.log(props.basket)
+// console.log(props.basket.map(obj => obj.type));
+
+/* const ticketInfo = props.basket.map((obj) => {
+  if (obj.type === "ticket") {
+    return obj.name
+  }
+}) */
+
   return (
     <div>
         <h2>Basket</h2>
+        <h3>Tickets:</h3>
+        <ul>
+          {props.basket.map((item) => (
+            <li key={item.name}>{item.name} x {item.amount}, {item.price}</li>
+          ))}
+        </ul>
         <button onClick={() => props.setShowForm(true)}>Checkout</button>
     </div>
   )
