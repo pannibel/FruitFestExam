e0390dea3113cd497ddeeae03e6632cfa8c39b68
HoodDoob <hubert.kurkiewicz@gmail.com>
2022-12-06 10:19:38 +0100

    clock for panni

--- src/tickets/Tickets.jsx
+++ src/tickets/Tickets.jsx
@@ -3,14 +3,22 @@ import { useState } from "react";
 import CheckoutForm from "./components/CheckoutForm";
 import TicketHeader from "./components/TicketHeader";
 import TicketList from "./components/TicketList";
+import ReactDOM from "react-dom";
+// npm install react-countdown --save
+import Countdown from "react-countdown";
 
 function Tickets() {
   const [showForm, setShowForm] = useState(false);
   const [basket, setBasket] = useState([]);
   const [guestNumber, setGuestNumber] = useState(0);
 
+  // ReactDOM.render(<Countdown date={Date.now() + 10000} />);
   return (
     <div>
+      <div>
+        {" "}
+        <Countdown date={Date.now() + 10000000} />
+      </div>
       {/* <TicketHeader /> */}
       <main>
         {!showForm && (
