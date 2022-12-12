c3f4444284fbbee7b59c88c1f775006a5a65ef05
HoodDoob <hubert.kurkiewicz@gmail.com>
2022-12-12 20:02:58 +0100

    i failied at life

--- src/lineup/components/Bandslist.jsx
+++ src/lineup/components/Bandslist.jsx
@@ -1,51 +1,87 @@
 import React from "react";
 import SingleBand from "./SingleBandList";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function Bandslist(props) {
-  const theInput = useRef(null)
-  // let sortedBands = props.bands.sort((a, b) => a.name - b.name);
-  console.log(props.bands);
+  const [searching, setSearching] = useState(false);
+  let searchedBands = [];
+  const theInput = useRef(null);
+
+  // let initArray = props.bands.map((band) => band);
+
   let sortedBands = props.bands.sort((a, b) => {
     return a.name > b.name ? 1 : -1;
   });
-  console.log(sortedBands);
-
 
   function searchList() {
-    let filteredBands = props.bands.filter((item) => {
-      if (item === theInput.current.value) {
-        return item
-      }
+    if (theInput.current.value != "") {
+      setSearching(true);
+      searchedBands = sortedBands.filter((band) =>
+        band.name.includes(theInput.current.value)
+      );
+
+      console.log(searchedBands);
+    } else {
+      setSearching(false);
     }
-  )
-    
-    console.log(theInput.current.value)
-    console.log(filteredBands)
   }
 
-  // filteredList = filteredList.sort(function (a, b) {
-  //   return a.props.band.localeCompare(b.firstName);
-  // })
+  // ! ! ! copy
+  // function searchList() {
+  //   sortedBands.map((band) => {
+  //     if (band.name.includes(theInput.current.value)) {
+  //       // sortedBands.filter(band);
+  //       // console.log(sortedBands.filter(band));
+  //       console.log(band);
+  //     }
+  //   });
+  // }
+  // ! ! ! copy
+  // ! it's here
+  // function searchList() {
+  //   if (theInput.current.value != "") {
+  //     setSearching(true);
+  //     let filtered = initArray.filter((band) => {
+  //       band.name.includes(theInput.current.value);
+  //     });
+  //     console.log(filtered);
+
+  //     sortedBands = sortedBands.filter((band) =>
+  //       band.name.includes(theInput.current.value)
+  //     );
+  //       sortedBands.push(searchedBands);
+  //     } else {
+  //       setSearching(false);
+  //   }
+  // }
+  // ! ! !
   return (
     <div className="bandsCont">
       <img
         className="logo_small"
         src="../src/assets/logo.svg"
-        alt="big logo of the festival"></img>
+        alt="big logo of the festival"
+      ></img>
 
       <div className="searchbarCont">
-        <input ref={theInput} type="text" name="searchbar" placeholder="search..." />
+        <input
+          ref={theInput}
+          type="text"
+          name="searchbar"
+          placeholder="search..."
+          // onInput={searchList}
+        />
         <button
           className="searchBtn"
           button-name="search"
-          onClick={searchList}></button>
+          onClick={searchList}
+        ></button>
       </div>
 
       <div className="bandListBox">
-        {sortedBands.map((band, i) => (
-          <SingleBand data={band} key={i} />
-        ))}
+        {!searching
+          ? sortedBands.map((band, i) => <SingleBand data={band} key={i} />)
+          : searchedBands.map((band, i) => <SingleBand data={band} key={i} />)}
       </div>
     </div>
   );
