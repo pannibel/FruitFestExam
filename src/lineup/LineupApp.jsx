0e80140570fdf65fa3b0a913323f867931939393
HoodDoob <hubert.kurkiewicz@gmail.com>
2022-12-20 13:51:21 +0100

    dfgdfg

--- src/lineup/LineupApp.jsx
+++ src/lineup/LineupApp.jsx
@@ -8,6 +8,13 @@ import CurrentBand from "./components/CurrentBand";
 import Burger from "./components/Burger";
 //npm i react-creditcard-validator
 //npm i react-burger-menu
+// !===============================================
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+// !===============================================
 
 function Lineup() {
   const [lineUpPage, setLineUpPage] = useState(1);
@@ -16,6 +23,96 @@ function Lineup() {
   const [singleBandState, setSingleBandState] = useState(false);
   const [openedBand, setOpenedband] = useState();
   const [bands, setBands] = useState([]);
+
+  // !==================================
+  useEffect(() => {
+    // laoding the scene
+    const scene = new THREE.Scene();
+
+    // supposedly background img loader
+    const loader = new THREE.TextureLoader();
+    loader.setCrossOrigin("");
+    const bgTexture = loader.load(function (texture) {
+      var img = texture.image;
+      bgWidth = img.width;
+      bgHeight = img.height;
+      resize();
+    });
+    scene.background = bgTexture;
+    bgTexture.wrapS = THREE.MirroredRepeatWrapping;
+    bgTexture.wrapT = THREE.MirroredRepeatWrapping;
+
+    // my uplaoded 3d o
+
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
+    const glftLoader = new GLTFLoader();
+    glftLoader.setDRACOLoader(dracoLoader);
+    let loadedModel;
+    glftLoader.load("./../src/mountain_4.glb", (gltfScene) => {
+      loadedModel = gltfScene;
+      // console.log(loadedModel);
+      gltfScene.scene.rotation.x = Math.PI / 8;
+      gltfScene.scene.position.y = -10;
+      gltfScene.scene.scale.set(5, 5, 5);
+      scene.add(gltfScene.scene);
+    });
+    // camera
+    const camera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    );
+    camera.position.z = 96;
+    const canvas = document.getElementById("myThreeJsCanvas");
+
+    // renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true,
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // added ambient light
+    const ambientLight = new THREE.AmbientLight(0xffffff, 6);
+    ambientLight.castShadow = true;
+    scene.add(ambientLight);
+
+    // added spotlight
+    const spotLight = new THREE.SpotLight(0xffffff, 1);
+    spotLight.castShadow = true;
+    spotLight.position.set(10, 64, 32);
+    scene.add(spotLight);
+
+    // const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // scene.add(boxMesh);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    const stats = Stats();
+    document.body.appendChild(stats.dom);
+
+    const animate = () => {
+      // setTimeout(function () {
+      //   requestAnimationFrame(animate);
+      // }, 1000 / 30);
+      if (loadedModel) {
+        loadedModel.scene.rotation.y += 0.01;
+      }
+      // boxMesh.rotation.y += 0.01;
+      stats.update();
+      renderer.render(scene, camera);
+      // renderer.render(scene, camera);
+      window.requestAnimationFrame(animate);
+    };
+    animate();
+
+    // scene.add(boxMesh);
+  }, []);
+  // !==================================
 
   // ? function to change the background
   // if (lineUpPage == 1) {
@@ -88,79 +185,83 @@ function Lineup() {
   }
 
   return (
-    <div className="appCont">
-      {lineUpPage == 1 ? (
-        <CurrentBand currentBand={currentBand} bands={bands} />
-      ) : (
-        ""
-      )}
-      {lineUpPage == 2 ? (
-        <Bandslist
-          bands={bands}
-          openOneBand={openOneBand}
-          openedBand={openedBand}
-          singleBandState={singleBandState}
-          setSingleBandState={setSingleBandState}
-        />
-      ) : (
-        ""
-      )}
-      {lineUpPage == 3 ? (
-        <Schedule
-          currentBand={currentBand}
-          bands={bands}
-          openOneBand={openOneBand}
-          openedBand={openedBand}
-          singleBandState={singleBandState}
-          setSingleBandState={setSingleBandState}
-        />
-      ) : (
-        ""
-      )}
+    <>
+      {" "}
+      <canvas id="myThreeJsCanvas" className="test3D" />
+      <div className="appCont">
+        {lineUpPage == 1 ? (
+          <CurrentBand currentBand={currentBand} bands={bands} />
+        ) : (
+          ""
+        )}
+        {lineUpPage == 2 ? (
+          <Bandslist
+            bands={bands}
+            openOneBand={openOneBand}
+            openedBand={openedBand}
+            singleBandState={singleBandState}
+            setSingleBandState={setSingleBandState}
+          />
+        ) : (
+          ""
+        )}
+        {lineUpPage == 3 ? (
+          <Schedule
+            currentBand={currentBand}
+            bands={bands}
+            openOneBand={openOneBand}
+            openedBand={openedBand}
+            singleBandState={singleBandState}
+            setSingleBandState={setSingleBandState}
+          />
+        ) : (
+          ""
+        )}
 
-      <div className="NavBarCont">
-        <button
-          onClick={() => changePageApp(1)}
-          className={lineUpPage === 1 ? checkPageApp("home") : "homeOff"}
-          value="home"
-        ></button>
-        <button
-          onClick={() => changePageApp(2)}
-          className={lineUpPage === 2 ? checkPageApp("bands") : "bandsOff"}
-          value="bands"
-        ></button>
-        <button
-          onClick={() => changePageApp(3)}
-          className={
-            lineUpPage === 3 ? checkPageApp("schedule") : "scheduleOff"
-          }
-          value="schedule"
-        ></button>
-      </div>
-      {/* <Bandslist bands={bands} /> */}
+        <div className="NavBarCont">
+          <button
+            onClick={() => changePageApp(1)}
+            className={lineUpPage === 1 ? checkPageApp("home") : "homeOff"}
+            value="home"
+          ></button>
+          <button
+            onClick={() => changePageApp(2)}
+            className={lineUpPage === 2 ? checkPageApp("bands") : "bandsOff"}
+            value="bands"
+          ></button>
+          <button
+            onClick={() => changePageApp(3)}
+            className={
+              lineUpPage === 3 ? checkPageApp("schedule") : "scheduleOff"
+            }
+            value="schedule"
+          ></button>
+        </div>
+        {/* <Bandslist bands={bands} /> */}
 
-      {!burgerState && (
-        <button
-          onClick={() => openBurger()}
-          className="burger_off"
-          button-name="openburg"
-        >
-          {" "}
-        </button>
-      )}
-      {burgerState && (
-        <>
+        {!burgerState && (
           <button
             onClick={() => openBurger()}
-            className="burger_on"
-            button-name="klosburg"
+            className="burger_off"
+            button-name="openburg"
           >
             {" "}
           </button>
-          <Burger />
-        </>
-      )}
-    </div>
+        )}
+        {burgerState && (
+          <>
+            <button
+              onClick={() => openBurger()}
+              className="burger_on"
+              button-name="klosburg"
+            >
+              {" "}
+            </button>
+            <Burger />
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
