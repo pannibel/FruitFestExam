8a36a5ebe90ea4ec0cfb1e8797166f1705633fca
HoodDoob <hubert.kurkiewicz@gmail.com>
2023-01-15 17:57:11 +0100

    hjk

--- src/tickets/components/TicketList.jsx
+++ src/tickets/components/TicketList.jsx
@@ -5,19 +5,6 @@ import Products from "./Products";
 import { confirmReservation } from "../../database";
 
 function TicketList(props) {
-
-  /* different view for desktop and mobile(from 1100px) */
-  const [windowSize, setWindowSize] = useState(window.innerWidth > 1110);
-
-  const updateMedia = () => {
-    setWindowSize(window.innerWidth > 1110);
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", updateMedia);
-    return () => window.removeEventListener("resize", updateMedia);
-  });
-
   const [spotAdded, setSpotAdded] = useState(false);
   const [campingSpots, setCampingSpots] = useState([]);
   const [curCampBtn, setCurCampBtn] = useState();
@@ -85,29 +72,7 @@ function TicketList(props) {
 
   return (
     <>
-    {!windowSize && (
-    <div className="mobileBasket">
-    <Basket
-          setIdValue={props.setIdValue}
-          idValue={props.idValue}
-          campingSpots={campingSpots}
-          count={count}
-          setCount={setCount}
-          setShowForm={props.setShowForm}
-          basket={props.basket}
-          removeFromBasket={removeFromBasket}
-          setSpotAdded={setSpotAdded}
-          spotAdded={spotAdded}
-          setGuestNumber={props.setGuestNumber}
-          guestNumber={props.guestNumber}
-          curCampBtn={curCampBtn}
-          setCurCampBtn={setCurCampBtn}
-          handleStartTimer={props.handleStartTimer}
-          isTimerRunning={props.isTimerRunning}
-          windowSize={windowSize}
-        />
-    </div>
-    )}
+      {" "}
       <div className="ticketlist">
         <Products
           addToBasket={addToBasket}
@@ -126,29 +91,27 @@ function TicketList(props) {
           handleStartTimer={props.handleStartTimer}
           isTimerRunning={props.isTimerRunning}
           windowSize={windowSize}
-
-        />
-        {windowSize && (
-          <Basket
-            setIdValue={props.setIdValue}
-            idValue={props.idValue}
-            campingSpots={campingSpots}
-            count={count}
-            setCount={setCount}
-            setShowForm={props.setShowForm}
-            basket={props.basket}
-            removeFromBasket={removeFromBasket}
-            setSpotAdded={setSpotAdded}
-            spotAdded={spotAdded}
-            setGuestNumber={props.setGuestNumber}
-            guestNumber={props.guestNumber}
-            curCampBtn={curCampBtn}
-            setCurCampBtn={setCurCampBtn}
-            handleStartTimer={props.handleStartTimer}
-            isTimerRunning={props.isTimerRunning}
-            windowSize={windowSize}
-          />
-        )}
+        />{" "}
+        {/* <div className="fixedBasket"> */}
+        <Basket
+          setIdValue={props.setIdValue}
+          idValue={props.idValue}
+          campingSpots={campingSpots}
+          count={count}
+          setCount={setCount}
+          setShowForm={props.setShowForm}
+          basket={props.basket}
+          removeFromBasket={removeFromBasket}
+          setSpotAdded={setSpotAdded}
+          spotAdded={spotAdded}
+          setGuestNumber={props.setGuestNumber}
+          guestNumber={props.guestNumber}
+          curCampBtn={curCampBtn}
+          setCurCampBtn={setCurCampBtn}
+          handleStartTimer={props.handleStartTimer}
+          isTimerRunning={props.isTimerRunning}
+        />{" "}
+        {/* </div> */}
       </div>
     </>
   );
